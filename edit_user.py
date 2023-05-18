40c22b006b2c777532ebb25560a9688bdb110d8e
cyber-phoon <ww-www-96@mail.ru>
2023-05-18 21:17:13 +0300

    Кастомизация кнопок, лейблов

--- edit_user.py
+++ edit_user.py
@@ -45,14 +45,16 @@ class Edit_Windows():
         # создаем новое окно
         edit_win = tk.Toplevel()
         edit_win.title("Редактирование данных")
+        bold_font = ("Tahoma", 10, "bold")
+        edit_win.configure(bg="light blue")
         # создаем поля для ввода новых данных
-        tk.Label(edit_win, text="Имя:").grid(row=0, column=0)
+        tk.Label(edit_win, text="Имя:", font=bold_font, bg="#87cefa", fg="black").grid(row=0, column=0)
         self.entry_new_first_name = tk.Entry(edit_win)
         self.entry_new_first_name.grid(row=0, column=1)
-        tk.Label(edit_win, text="Фамилия:").grid(row=1, column=0)
+        tk.Label(edit_win, text="Фамилия:", font=bold_font, bg="#87cefa", fg="black").grid(row=1, column=0)
         self.entry_new_last_name = tk.Entry(edit_win)
         self.entry_new_last_name.grid(row=1, column=1)
-        tk.Label(edit_win, text="Отчество:").grid(row=2, column=0)
+        tk.Label(edit_win, text="Отчество:", font=bold_font, bg="#87cefa", fg="black").grid(row=2, column=0)
         self.entry_new_patronymic = tk.Entry(edit_win)
         self.entry_new_patronymic.grid(row=2, column=1)
         #tk.Label(edit_win, text="Фото:").grid(row=3, column=0)
@@ -67,8 +69,8 @@ class Edit_Windows():
         #print(entry_new_last_name)
         #print(entry_new_patronymic)
         # создаем кнопку для того чтобы сделать фотографию и сохранить
-        tk.Button(edit_win, text="Сделать фотографию", command=self.process_camera_photo).grid()
-        tk.Button(edit_win, text="Изменить введенное ФИО", command=lambda: self.change_data(self.entry_first_name.get(), self.entry_last_name.get(), self.entry_patronymic.get(), self.entry_new_first_name.get(), self.entry_new_last_name.get(), self.entry_new_patronymic.get())).grid()
+        tk.Button(edit_win, text="Сделать фотографию", command=self.process_camera_photo, font=bold_font, bg="#87cefa", fg="black").grid()
+        tk.Button(edit_win, text="Изменить введенное ФИО", command=lambda: self.change_data(self.entry_first_name.get(), self.entry_last_name.get(), self.entry_patronymic.get(), self.entry_new_first_name.get(), self.entry_new_last_name.get(), self.entry_new_patronymic.get()), font=bold_font, bg="#87cefa", fg="black").grid()
 
 
 
